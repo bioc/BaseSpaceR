34ffeca1dbfb68f7adbe9bbbddc462b8d49a638d
Adrian Alexa <alexa@mpi-inf.mpg.de>
2013-04-03 17:08:11 +0000

    Updated NEWS file and minor edits to the documentation
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BaseSpaceR@75239 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,8 @@
   packageStartupMessage("BaseSpaceR version ", packageVersion("BaseSpaceR"), 
                         ", ?BaseSpaceR for help")
   
-  options(stringsAsFactors = FALSE)
-
+  ## options(stringsAsFactors = FALSE)
+  
   ## address bug in Windows RCurl
   if(.Platform$OS.type == "windows")
     options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"), verbose = TRUE))
